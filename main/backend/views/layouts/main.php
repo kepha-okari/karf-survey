987874bc68e880ada630043d1eb9435710fe6ff1
kepha-okari <kephaokari@gmail.com>
2020-06-17 10:47:00 +0300

    layouts

--- main/backend/views/layouts/main.php
+++ main/backend/views/layouts/main.php
@@ -26,12 +26,7 @@ DashboardAsset::register($this);
 <div class="wrapper">
   <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini">MS</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg">Mswali</span>
-    </a>
+
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation"">
       <!-- Sidebar toggle button-->
@@ -47,24 +42,7 @@ DashboardAsset::register($this);
               <i class="fa fa-envelope-o"></i>
               <span class="label label-success"></span>
             </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 4 messages</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li><!-- start message -->
-                    <a href="#">
-                      <div class="pull-left">
-                        <img src="dist/img/avatar.png" class="img-circle" alt="User Image">
-                      </div>
-                    </a>
-                  </li>
-                  <!-- end message -->
-             
-                </ul>
 
-              </li>
-            </ul>
           </li>
           <!-- Notifications: style can be found in dropdown.less -->
           <li class="dropdown notifications-menu">
@@ -111,7 +89,7 @@ DashboardAsset::register($this);
     </nav>
   </header>
     <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
+  <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
       <!-- Sidebar user panel -->
@@ -119,107 +97,26 @@ DashboardAsset::register($this);
         <div class="pull-left image">
           <img src="dist/img/avatar.png" class="img-circle" alt="User Image">
         </div>
-        <div class="pull-left info">
-          <p>Administrator</p>
+        <div class="pull-right info">
           <a href="#"><i class="fa fa-circle text-success"></i> Active</a>
         </div>
       </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
+  
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="active treeview">
           <a href="#">
-           <span>MAIN NAVIGATION</span> <i class="fa fa-angle-left pull-right"></i>
+          <span>MAIN NAVIGATION</span> <i class="fa fa-angle-left pull-right"></i>
           </a>
-          <ul class="treeview-menu">
-            <li class="active"><a href="http://104.236.11.199/mswali/mswali_app/backend/web/index.php?r=site/index"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-            <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=surveys/index"><i class="fa fa-question"></i> Surveys</a></li>
-            <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=questions/index"><i class="fa fa-question"></i> Questions</a></li>
-            <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=options/index"><i class="fa fa-question"></i> Options</a></li>
-            <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=responses/index"><i class="fa fa-question"></i> Responses</a></li>
-          
-          </ul>
+            <ul class="treeview-menu">
+                <li class="active"><a href="http://104.236.11.199/mswali/mswali_app/backend/web/index.php?r=site/index"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=surveys/index"><i class="fa fa-microscope"></i> Surveys</a></li>
+                <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=questions/index"><i class="fa fa-"></i> Questions</a></li>
+                <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=options/index"><i class="fa fa-"></i> Options</a></li>
+                <li><a href="http://104.236.11.199/questionnaire/main/backend/web/index.php?r=responses/index"><i class="fa fa-"></i> Responses</a></li>
+            </ul>
         </li>
-      
-        <li class="header">GAMES DATA
-            <small class="label pull-right bg-green"></small>
-        </li>
-        <li class="active treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="http://161.35.6.91/mswali/mswali_app/backend/web/index.php?r=questions/index"><i class="fa fa-question"></i> Questions</a></li>
-          </ul>
-        </li>
-
-        <li class="header">WALLETS & TRANSACTIONS
-            <small class="label pull-right bg-green"></small>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-          </ul>
-        </li>
-
-        <li class="header">REPORTS & STATISTICS
-            <small class="label pull-right bg-green"></small>
-        </li>
-
-      
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-laptop"></i>
-            <span>UI Elements</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-            <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
-          </ul>
-        </li>
-   
-
-      
-     
-        <li class="header">TOOLS</li>
-        <li><a href="#"><i class="fa fa-cloud-upload text-blue"></i> <span>UPLOAD TRIVIA ITEMS</span></a></li>
-        <li><a href="http://161.35.6.91/mswali/mswali_app/backend/web/index.php?r=game-sessions/create"><i class="fa fa-calendar text-yellow"></i> <span>SCHEDULE A GAME</span></a></li>
-        <li><a href="#"><i class="fa fa-flag text-aqua"></i> <span>FLAG QUESTIONS</span></a></li>
       </ul>
     </section>
     <!-- /.sidebar -->
