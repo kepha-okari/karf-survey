b024b64142f767f2978566cf955fa4fa34e5b599
kepha-okari <kephaokari@gmail.com>
2020-06-19 14:16:53 +0300

    add survey choice drop down

--- main/backend/views/questions/_form.php
+++ main/backend/views/questions/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use backend\models\Surveys;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Questions */
@@ -11,7 +13,11 @@ use yii\widgets\ActiveForm;
 <div class="questions-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+    <?= $form->field($model, 'survey_id')->dropDownList(
+            #ArrayHelper::map(Questions::find()->where(['survey_id' => 1])->orderBy('title')->asArray()->all(),'id','title'),
+            ArrayHelper::map(Surveys::find()->orderBy('survey_name')->asArray()->all(),'id','survey_name'),
+            ['prompt'=>'Select Survey']
+        ) ?>
     <?= $form->field($model, 'survey_id')->textInput() ?>
 
     <?= $form->field($model, 'state')->textInput(['maxlength' => true]) ?>
